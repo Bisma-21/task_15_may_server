d5175bdb5f343c0c1d8e46d63dd693432cec9a02
bisma <bismahmanxoor1@gmail.com>
2026-05-16 15:59:37 +0530

    allow all requests

--- src/index.js
+++ src/index.js
@@ -11,12 +11,7 @@ import { notFound, errorHandler } from "./middleware/error.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_ORIGIN || "*",
-    credentials: false,
-  }),
-);
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
 
